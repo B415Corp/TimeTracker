019f4907b7efd25e879de60bf37366a6b64d577c
Алексей <101291963+Alexeykoh@users.noreply.github.com>
2025-06-08 00:29:47 +0300

    feat: Introduce EditableFieldFormFeature for editable input with validation
    - Add EditableFieldFormFeature component for an editable text field with validation using Zod.
    - Refactor EditableFieldForm widget to utilize the new feature component, improving code organization and maintainability.

---
+++ apps/frontend/src/features/forms/EditableFieldFormFeature.tsx
@@ -0,0 +1,79 @@
+import { Input } from "@ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@ui/form";
+
+interface Props {
+  value: string;
+  action: (value: string) => void;
+}
+
+const fieldSchema = z.object({
+  value: z
+    .string()
+    .nonempty("Поле не должно быть пустым")
+    .max(20, "Максимум 20 символов"),
+});
+
+type fieldSchemaFormValues = z.infer<typeof fieldSchema>;
+
+/**
+ * Feature-компонент: редактируемое текстовое поле с валидацией и экшеном
+ */
+export function EditableFieldFormFeature({ value, action }: Props) {
+  const form = useForm<fieldSchemaFormValues>({
+    resolver: zodResolver(fieldSchema),
+    defaultValues: { value },
+  });
+
+  useEffect(() => {
+    form.reset({ value });
+  }, [value, form]);
+
+  const watchedValue = form.watch("value");
+  useEffect(() => {
+    if (watchedValue !== value) {
+      form.setValue("value", watchedValue, { shouldDirty: true });
+    }
+  }, [watchedValue, value, form]);
+
+  async function onSubmit({ value }: fieldSchemaFormValues) {
+    const drt = form.formState.isDirty;
+    if (drt) {
+      action(value);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="value"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  onBlurCapture={form.handleSubmit(onSubmit)}
+                  className="border-none"
+                  type="text"
+                  placeholder="Email"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+} 
